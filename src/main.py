2cfb20c33eacd268c42433b5ab18a469bc73e67e
t24ber <t24ber@t24bers-MBP.lan>
2025-10-11 17:55:52 -0400

    Added resize_file()

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import os
+from PIL import Image
 
 def rename_file(filename, new_filename):
 	if len(new_filename.split(".")) >= 2:
 		os.rename(f"input/{filename}", f"output/{new_filename}")
 	else:
 		print(f"extension missing: {new_filename}")
+
+def resize_file(filename, width, height):
+	with Image.open(f"input/{filename}") as image:
+		image.resize((width, height)).save(f"output/{filename}")
