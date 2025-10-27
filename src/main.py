2c6ab245177d86ba3b456369dc84d7f00034d4f9
Tyler <t24ber@gmail.com>
2025-10-26 20:30:44 -0400

    Removed file extension check

--- src/main.py
+++ src/main.py
@@ -3,11 +3,8 @@ from PIL import Image
 
 
 def rename_file(filename, new_filename):
-	if len(new_filename.split(".")) >= 2:
-		os.rename(f"input/{filename}", f"output/{new_filename}")
-		print(f"====> Renamed {filename} to {new_filename}")
-	else:
-		print(f"extension missing: {new_filename}")
+	os.rename(f"input/{filename}", f"output/{new_filename}")
+	print(f"====> Renamed {filename} to {new_filename}")
 
 def resize_image(filename, new_dimensions):
 
